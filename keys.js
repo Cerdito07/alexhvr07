cf28b8a43ed52524fdfa174a416ebae902929d96
Cerdito <139197476+Cerdito07@users.noreply.github.com>
2025-06-11 16:25:31 +0200

    keys.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+  binId: "68422a498960c979a5a5bfad",
+  apiKey: "$2a$10$b9SFzv4ViAihnX0LrBzgZ.1tD2kUECvIpvZPSxeTafyckUIQohZLu",
+  webhookUrl: "https://discord.com/api/webhooks/1379196950548840562/dbcNuGow4IcIdN8Fj1Due9d_e-PDm43i3AWxYUQZ4Zhyp5J_oe4vk6yfrprOqsPZvCis"
+};
