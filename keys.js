de4a5cce1b0aa0b6c105c78a13a0cfa69a3d2563
Cerdito <139197476+Cerdito07@users.noreply.github.com>
2025-08-27 15:35:16 +0200

    keys.js

--- keys.js
+++ keys.js
@@ -2,5 +2,5 @@
 const config = {
   binId: "68422a498960c979a5a5bfad",
   apiKey: "$2a$10$b9SFzv4ViAihnX0LrBzgZ.1tD2kUECvIpvZPSxeTafyckUIQohZLu",
-  webhookUrl: "https://discord.com/api/webhooks/1410254728402698341/cZ_X1jAcMRFJEbf2cRX6Lq0TuEc9veiBdjlafV7W8D9EVBTmqpd2CTLhKwZ4nxG5KT6i"
+  webhookUrl: "https://discord.com/api/webhooks/1410254728402698341/cZ_X1jAcMRFJEbf2cRX6Lq0TuEc9veiBdjlafV7W8D9EVBTmqpd2CTLhKwZ4nxG5KT6i" 
 };
